dc5317033b4bf7115a4f36f758dbccfd80e82b40
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-01-04 18:39:40 +0300

    eklenti

--- src/org/csystem/cmdpromptapp/App.java
+++ src/org/csystem/cmdpromptapp/App.java
@@ -1,7 +1,6 @@
 package org.csystem.cmdpromptapp;
 
 import org.csystem.database.CommandPrompt;
-import org.csystem.entity.CommandInfo;
 
 public class App {
     public static void main(String[] args)
